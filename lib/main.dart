3f0a341e432b33546f785bfdd5c3ea1df87bab65
HaoNguyen <nguyenthi.hao@rights-s.jp>
2024-08-01 13:38:37 +0700

    change release assets path

--- lib/main.dart
+++ lib/main.dart
@@ -70,26 +70,21 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return UpdatWindowManager(
       getLatestVersion: () async {
-        // Github gives us a super useful latest endpoint, and we can use it to get the latest stable release
         final data = await http.get(Uri.parse(
-          "https://api.github.com/repos/fluttertools/sidekick/releases/latest",
+          "https://api.github.com/repos/nguyenthihao-rightsvn/flutter_updat_test/releases/latest",
         ));
 
         // Return the tag name, which is always a semantically versioned string.
         return jsonDecode(data.body)["tag_name"];
       },
       getBinaryUrl: (version) async {
-        // Github also gives us a great way to download the binary for a certain release (as long as we use a consistent naming scheme)
-
-        // Make sure that this link includes the platform extension with which to save your binary.
-        // If you use https://exapmle.com/latest/macos for instance then you need to create your own file using `getDownloadFileLocation`
-        return "https://github.com/fluttertools/sidekick/releases/download/$version/sidekick-${Platform.operatingSystem}-$version.$platformExt";
+        return "https://github.com/nguyenthihao-rightsvn/flutter_updat_test/releases/download/$version/${Platform.operatingSystem}-$version.$platformExt";
       },
-      appName: "Updat Example", // This is used to name the downloaded files.
+      appName: "Updat Test", // This is used to name the downloaded files.
       getChangelog: (_, __) async {
         // That same latest endpoint gives us access to a markdown-flavored release body. Perfect!
         final data = await http.get(Uri.parse(
-          "https://api.github.com/repos/fluttertools/sidekick/releases/latest",
+          "https://api.github.com/repos/nguyenthihao-rightsvn/flutter_updat_test/releases/latest",
         ));
         return jsonDecode(data.body)["body"];
       },
